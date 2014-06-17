ab0bf8ddb1f98ef8b39adaed61655428b3300652
Gyumin <gmsim@likelion.net>
2014-06-17 18:05:08 +0900

    master 브랜치가 선형 로그인지 확인하도록 함
    Command 클래스에 verify라는 메서드를 만들고
    생성자에서 이를 불러서 체크함

--- lib/toygit.rb
+++ lib/toygit.rb
@@ -5,6 +5,19 @@ module ToyGit
     def initialize
       path = Rugged::Repository.discover('.')
       @repo = Rugged::Repository.new(path)
+      raise 'Invalid ToyGit Repository' unless verify
+    end
+
+    private
+
+    def verify
+      master = @repo.ref('refs/heads/master')
+      walker = Rugged::Walker.new(@repo)
+      walker.push master.target
+      walker.each do |commit|
+        return false if commit.parents.count > 1
+      end
+      true
     end
   end
 end
