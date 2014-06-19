7cd98b07d91e3ba12febcf81bd2f706845341313
Gyumin <gmsim@likelion.net>
2014-06-19 19:29:14 +0900

    Command.verify에서 커밋 메시지로부터 chapter와 step을 파싱해놓기
    "[chapter] step" 형태를 파싱해서 @commits에 저장해놓는다.

--- lib/toygit.rb
+++ lib/toygit.rb
@@ -11,12 +11,25 @@ module ToyGit
     private
 
     def verify
+      commits = []
+
       master = @repo.ref('refs/heads/master')
       walker = Rugged::Walker.new(@repo)
       walker.push master.target
       walker.each do |commit|
         return false if commit.parents.count > 1
+        summary = commit.message.lines[0]
+        if summary =~ /\[([[:print:]]+)\][[:space:]]?([[:print:]]*)/
+          chapter = $1
+          step = $2
+        else
+          chapter = ''
+          step = summary
+        end
+        commits.push({chapter: chapter, step: step, rugged_commit: commit})
       end
+
+      @commits = commits.reverse
       true
     end
   end
